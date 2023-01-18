30116789f4c157bed0758b69ec480c2e2d2326c6
Alessandro <less013015@gmail.com>
2023-01-18 08:46:18 +0100

    Añadiendo el resto de operaciones del CRUD a migrations

--- src/components/react-admin/admin.js
+++ src/components/react-admin/admin.js
@@ -1,11 +1,11 @@
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from 'components/react-admin/users';
-import { MigrationList } from 'components/react-admin/migrations';
 import { PostList, PostEdit, PostCreate } from 'components/react-admin/posts';
-import MigrationIcon from '@mui/icons-material/Storage';
+import { MigrationList, MigrationEdit, MigrationCreate } from 'components/react-admin/migrations';
 import PostIcon from '@mui/icons-material/Book';
 import UserIcon from '@mui/icons-material/Group';
+import MigrationIcon from '@mui/icons-material/Storage';
 
 import { AdminLayout } from 'components/react-admin/adminLayout';
 
@@ -17,8 +17,9 @@ const RAdmin = () => (
     dataProvider={dataProvider}
     layout={AdminLayout}
   >
-    <Resource name="migrations" list={MigrationList} icon={MigrationIcon} recordRepresentation="migration" />
-    {/* <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} /> */}
+    <Resource name="migrations"
+      list={MigrationList} icon={MigrationIcon} edit={MigrationEdit} create={MigrationCreate}/>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name" />
   </Admin>
 )
--- src/components/react-admin/migrations.js
+++ src/components/react-admin/migrations.js
@@ -1,24 +1,70 @@
 // in src/components/react-admin/migrations.js
 import { useMediaQuery } from '@mui/material';
-import { List, SimpleList, Datagrid, TextField } from 'react-admin';
+
+import {
+  List,
+  SimpleList,
+  Datagrid,
+  TextField,
+  EditButton,
+  Edit,
+  Create,
+  SimpleForm,
+  TextInput
+} from 'react-admin';
+
+import { useRecordContext} from 'react-admin';
+
+const postFilters = [
+  <TextInput source="q" label="Search" alwaysOn />
+];
 
 export const MigrationList = () => {
   const isSmall = useMediaQuery((theme) => theme.breakpoints.down('sm'));
   return (
-    <List>
+    <List filters={postFilters}>
       {isSmall ? (
         <SimpleList
           primaryText={(record) => record.id}
           secondaryText={(record) => record.migration}
           tertiaryText={(record) => record.batch}
-        />
+        >
+
+          <EditButton />
+        </SimpleList>
       ) : (
         <Datagrid rowClick="edit">
           <TextField source="id" />
           <TextField source="migration" />
           <TextField source="batch" />
+          <EditButton />
         </Datagrid>
       )}
     </List>
   );
 };
+
+const MigrationTitle = () => {
+const record = useRecordContext();
+return <span>Migration {record ? `"${record.migration}"` : ''}</span>;
+};
+
+export const MigrationEdit = () => (
+  <Edit title={<MigrationTitle />}>
+  <SimpleForm>
+      <TextInput source="id" disabled />
+      <TextInput source="migration" />
+      <TextInput source="batch" />
+  </SimpleForm>
+  </Edit>
+);
+
+export const MigrationCreate = () => (
+  <Create>
+      <SimpleForm>
+      <TextInput source="id" disabled />
+      <TextInput source="migration" />
+      <TextInput source="batch" />
+      </SimpleForm>
+  </Create>
+  );
