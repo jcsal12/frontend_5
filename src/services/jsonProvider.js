d24340927294a0ef8f90b251dc03a9d74326ad10
Alessandro <less013015@gmail.com>
2023-02-24 21:28:31 +0100

    fetch a endpoint libros

--- src/services/jsonProvider.js
+++ src/services/jsonProvider.js
@@ -1,5 +1,5 @@
 export function jsonProvider() {
-  return fetch('http://encuentro.com/api/books')
+  return fetch('http://encuentro.com/api/libros')
     .then(response => response.json())
     .then(data => {
       let libros = data;
